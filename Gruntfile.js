2881f5148ddb3c927347a75249db6ada858a2796
mbrasci <mbrasci@caami.be>
2016-09-30 14:51:42 +0200

    Update ReadMe.md

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,16 @@
 module.exports = function (grunt) {
 
     grunt.initConfig({
-        clean: ['src/main/resources/static/build', 
+        clean: ['src/main/resources/static/build',
             './Reports/*.html',
             './Reports/*.json',
             './Reports/screenshot/*.png'
-        
-        ],
-        karma: {
-            unit: {
-                configFile: 'karma.conf.js'
-            }
-        }
+
+        ]
     });
 
-    grunt.loadNpmTasks('grunt-karma');
     grunt.loadNpmTasks('grunt-contrib-clean');
 
-    grunt.registerTask('build', ['clean','karma']);
+    grunt.registerTask('build', ['clean', 'karma']);
 
 };
